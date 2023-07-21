1373912f190d8a548304ecb06ab43a72db8e8372
javad noroozi <javadno.9514@gmail.com>
2023-07-21 16:41:16 +0330

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref } from "vue";
+
+const name = ref("javad");
+</script>
+
+<template>
+  <VTextField v-model="name" />
+
+  <div>{{ name }}</div>
+</template>
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+
+const vuetify = createVuetify();
+
+createApp(App).use(vuetify).mount("#app");
