27541e122abf3ea7c36a529c64a39eb73d56e75b
mjnoroozi.me <mjnoroozi.me@gmail.com>
2023-07-22 15:51:16 +0330

    fix bug

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
 import "vuetify/styles";
 import { createVuetify } from "vuetify";
 
-const vuetify = createVuetify();
+const myCustomLightTheme = {
+  dark: false,
+  colors: {
+    background: "#FFFFFF",
+    surface: "#FFFFFF",
+    primary: "#6200EE",
+    "primary-darken-1": "#3700B3",
+    secondary: "#03DAC6",
+    "secondary-darken-1": "#018786",
+    error: "#B00020",
+    info: "#2196F3",
+    success: "#4CAF50",
+    warning: "#FB8C00",
+  },
+};
 
-export { vuetify };
+const vuetify = createVuetify({
+  theme: {
+    defaultTheme: "driver",
+    themes: {
+      myCustomLightTheme,
+    },
+  },
+});
+
+export default vuetify;
