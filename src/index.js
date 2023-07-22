5db172d126a6d4222e62725f893f739f7b86f7e7
mjnoroozi.me <mjnoroozi.me@gmail.com>
2023-07-22 14:54:30 +0330

    fix bug

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
-// import { createApp } from "vue";
-// import App from "./App.vue";
-
 import "vuetify/styles";
 import { createVuetify } from "vuetify";
 
 const vuetify = createVuetify();
-
-// createApp(App).use(vuetify).mount("#app");
 
 export { vuetify };
